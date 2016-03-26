66409caca5842b31a3f38615b7bcc19cb48ccd1f
Shijia He <shijia0113@gmail.com>
2016-03-26 16:24:27 -0400

    abc

---
+++ src/main/java/com/sega/models/User.java
@@ -0,0 +1,27 @@
+package com.sega.models;
+
+/**
+ * Created by sega on 2016/3/26.
+ */
+public class User {
+    private String username;
+
+    private String password;
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+}
